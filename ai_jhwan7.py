14ff480e2e4e3d77eeb02226f0afd849d74ecf47
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-10 12:17:30 -0500

    add imports to libraries

--- ai_jhwan7.py
+++ ai_jhwan7.py
@@ -5,3 +5,13 @@ Created on Sun Jan 10 12:15:14 2021
 @author: jun_h
 """
 
+# import libraries
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from autograd import Variable
