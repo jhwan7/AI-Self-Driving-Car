68ddb0840c2f9e243f720aca1a0522d10bad99b9
jhwan7 <46652246+jhwan7@users.noreply.github.com>
2021-01-10 12:35:07 -0500

    create constructor for the AI architecture

--- ai_jhwan7.py
+++ ai_jhwan7.py
@@ -15,3 +15,24 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from autograd import Variable
+
+# create the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    # this can be arbitrarily chosen, 30 is most optimized size after multiple trials
+    hidden_size = 30
+    
+    def __init__(self, input_size, output_size):
+        # initialize object using nn.Module constructor
+        super(Network, self).__init__()
+        
+        # initialize the the input dimension size, and the output dimension size
+        self.input_size = input_size
+        self.output_size = output_size
+        
+        # connect the neurons from the inner layer to the hidden layer, "fc" means "Full Connection (Linear)" : all neurons of the input layer is connected to the hidden layer.
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        
+        # connect the neurons from the hidden layer to the output layer
+        self.fc2 = nn.Linear(hidden_size, output_size)
